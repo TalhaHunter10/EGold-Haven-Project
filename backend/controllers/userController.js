25865891b2fc74142aa737341d734f978c85c8db
TalhaHunter10 <talhaayub789@gmail.com>
2024-02-27 01:41:41 +0500

    backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,10 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+const Token = require("../models/tokenModel");
 const { response } = require("express");
+const sendEmail = require("../utils/sendEmail");
 
 const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "6h" })
@@ -212,6 +215,57 @@ const changePassword = asyncHandler(async(req, res) => {
     }
 });
 
+const forgotPassword = asyncHandler(async (req, res) => {
+    const {email} = req.body
+    const user = await User.findOne({email})
+
+    if(!user){
+        res.status(404)
+        throw new Error("This email is not registered with any account. Please Try Again");
+    }
+
+    let token = await Token.findOne({userId: user._id})
+    if(token){
+        await token.deleteOne();
+    }
+
+    let resetToken = crypto.randomBytes(32).toString("hex") + user._id
+
+    const hashedToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+
+    await new Token({
+        userId: user._id,
+        token: hashedToken,
+        createdAt: Date.now(),
+        expiresAt:Date.now() + 30 *(60 * 1000) //30 minutes
+    }).save()
+
+    const resetUrl = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`
+
+    const message = `
+      <h2>Hello ${user.name}</h2>
+      <p>Kindly click on the url below to reset your password</p>
+      <p>The link is valid for 30 minutes !!!</p>
+
+      <a href=${resetUrl} clicktracking=off>${resetUrl}</a>
+
+      <p>Regards...</p>
+      <p>Egold Haven Team</p>
+    `;
+
+    const subject = "Password Reset Request for Egold Haven Account";
+    const send_to = user.email
+    const sent_from = process.env.EMAIL_USER
+    
+    try{
+        await sendEmail(subject, message, send_to, sent_from)
+        res.status(200).json({success:true, message:"Reset Email Sent"})
+    }catch(error){
+        res.status(500)
+        throw new Error("Email could not be Sent. Please try again !")
+    }
+});
+
 module.exports = {
     registerUser,
     loginUser,
@@ -220,5 +274,6 @@ module.exports = {
     getUserStatus,
     loginStatus,
     updateUser,
-    changePassword
+    changePassword,
+    forgotPassword
 }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const protect = require("../middlewares/authmiddleware");
-const { registerUser, loginUser, logOut, getUser, getUserStatus, loginStatus, updateUser, changePassword } = require("../controllers/userController");
+const { registerUser, loginUser, logOut, getUser, getUserStatus, loginStatus, updateUser, changePassword, forgotPassword } = require("../controllers/userController");
 const router =  express.Router();
 
 router.post("/register", registerUser)
@@ -12,5 +12,6 @@ router.get("/getuserstatus", protect, getUserStatus);
 router.get("/loggedin" , loginStatus)
 router.patch("/updateuser",protect, updateUser)
 router.patch("/changepassword", protect, changePassword)
+router.post("/forgotpassword",forgotPassword);
 
 module.exports = router
